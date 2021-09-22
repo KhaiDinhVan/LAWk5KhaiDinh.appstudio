5dc744f8b6256a4c90235e4afe8e7d236fd524fa
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-09-22 15:09:11 -0500

    updated Cal

---
+++ forms/calc/calc.js
@@ -0,0 +1,9 @@
+let num1 = prompt('What is the first number:')
+let num2 = prompt('what is the second number:')
+
+function summedNumbers = calMath(num1, num2) {
+  let summedNumbers = number1 + number2
+  return summedNumbers
+
+}
+
